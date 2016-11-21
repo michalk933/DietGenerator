91710c1eedc7c92189d306a800a4c2107705f8b3
michalk933 <6pak.tv@gmail.com>
2016-11-21 13:42:36 +0100

    Create cursorAdapter for product

--- app/src/main/java/com/example/michal/myapplication/ProductListActivity.java
+++ app/src/main/java/com/example/michal/myapplication/ProductListActivity.java
@@ -1,6 +1,10 @@
 package com.example.michal.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.DialogInterface;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class ProductListActivity extends AppCompatActivity {
+    productArrayAdapter paa;
+    private Cursor cursor;
+    private ListView listView;
+
 
     private static Context context;
 
@@ -21,6 +33,26 @@ public class ProductListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ProductListActivity.context = getApplicationContext();
+        listView = (ListView)findViewById(R.id.productListView);
+
+
+        try {
+            SQLiteOpenHelper helper = new DB_Product(this);
+            SQLiteDatabase db = helper.getReadableDatabase();
+
+            Cursor cu = db.rawQuery("SELECT  * FROM PRODUCT", null);
+            productArrayAdapter p = new productArrayAdapter(this,cu,0);
+            listView.setAdapter(p);
+
+            cursor.close();
+            db.close();
+
+        }catch (SQLiteException e){
+            Toast.makeText(getApplicationContext(), "Błąd bazy dabych produktów", Toast.LENGTH_SHORT).show();
+
+        }
+
+
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
--- app/src/main/java/com/example/michal/myapplication/productArrayAdapter.java
+++ app/src/main/java/com/example/michal/myapplication/productArrayAdapter.java
@@ -1,10 +1,12 @@
 package com.example.michal.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,11 +15,48 @@ import java.util.List;
  * Created by michal on 20.11.2016.
  */
 
-public class productArrayAdapter extends ArrayAdapter<> {
-    
+public class productArrayAdapter extends CursorAdapter {
+    private LayoutInflater cursorInflater;
+
+
+    public productArrayAdapter(Context context, Cursor c,int flags) {
+        super(context, c,flags);
+    }
+
+
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return cursorInflater.from(context).inflate(R.layout.product_list,parent,false);
+    }
+//return LayoutInflater.from(context).inflate(R.layout.item_todo, parent, false);
+
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+
+
+
+        TextView nameListProductTextView = (TextView) view.findViewById(R.id.nameListProductTextView);
+        TextView kcalListProductTextView = (TextView) view.findViewById(R.id.kcalListProductTextView);
+        TextView igListProductTextView = (TextView) view.findViewById(R.id.igListProductTextView);
+        TextView WWListProductTextView = (TextView) view.findViewById(R.id.WWListProductTextView);
+        TextView BListProductTextView = (TextView) view.findViewById(R.id.BListProductTextView);
+        TextView TListProductTextView = (TextView) view.findViewById(R.id.TListProductTextView);
 
 
 
 
 
+
+        nameListProductTextView.setText(cursor.getString(cursor.getColumnIndexOrThrow("NAME")));
+        kcalListProductTextView.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow("KCAL_G"))));
+        igListProductTextView.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow("IG"))));
+        WWListProductTextView.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow("WW_G"))));
+        BListProductTextView.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow("B_G"))));
+        TListProductTextView.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow("T_G"))));
+
+
+    }
 }
